bcb46445d37462c13ff2c48ec6927a4fd90f5f38
DESKTOP-NBPOGKF\D.Nima <nimasalamitest@gmail.com>
2024-04-15 13:35:47 +0330

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Declare variables
+string name = string.Empty;
+int age = 0;
+int retiermentAge = 65;
+
+// Prompt the user for input
+Console.WriteLine("Please Enter your name:");
+name = Console.ReadLine();
+
+Console.WriteLine("Please enter your age: ");
+age = Convert.ToInt32(Console.ReadLine());
+
+// Process
+int workingYearsRemaining = retiermentAge - age;
+Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
+
+
