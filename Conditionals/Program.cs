7a8e4074c753c300171ef68931a586908c5d55b6
DESKTOP-NBPOGKF\D.Nima <nimasalamitest@gmail.com>
2024-04-17 11:25:55 +0330

    Explore Conditional statements

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -22,3 +22,18 @@ else
 string passStatus = grade < 50 ? "Fail" : "Pass";
 Console.WriteLine("Status: " + passStatus);
 Console.WriteLine("Thanks for using our app");
+
+Console.WriteLine("******** End of Process");
+
+Console.WriteLine("******** Start the program");
+
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+switch(dayOfWeek)
+{
+    case 1: 
+        Console.WriteLine("Sunday");
+        if(dayOfWeek == 0){
+            Console.WriteLine("Please enter 1 - 7");
+        }
+        break;
+}
